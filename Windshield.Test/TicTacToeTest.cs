ca4df66949208d9fb31390102f8e42d198478ecb
sparog.throstur@gmail.com <sparog.throstur@gmail.com@ed2c307c-5ce9-fec3-88ac-91bfb60aa462>
2013-05-08 15:32:44 +0000

    test prototypes

--- Windshield.Test/TicTacToeTest.cs
+++ Windshield.Test/TicTacToeTest.cs
@@ -34,11 +34,82 @@ namespace Windshield.Test
 		[TestMethod]
 		public void InsertSymbol()
 		{
-			game.InsertSymbol('T', 0);
+			game.InsertSymbol('1', 1);
+			game.InsertSymbol('2', 2);
+			game.InsertSymbol('3', 3);
+			game.InsertSymbol('4', 4);
+			game.InsertSymbol('5', 5);
+			game.InsertSymbol('6', 6);
+			game.InsertSymbol('7', 7);
+			game.InsertSymbol('8', 8);
+			game.InsertSymbol('0', 0);
 
-			Assert.AreEqual('T', game.grid[0, 0]);
+			Assert.AreEqual('0', game.grid[0, 0]);
+			Assert.AreEqual('1', game.grid[1, 0]);
+			Assert.AreEqual('2', game.grid[2, 0]);
+			Assert.AreEqual('3', game.grid[0, 1]);
+			Assert.AreEqual('4', game.grid[1, 1]);
+			Assert.AreEqual('5', game.grid[2, 1]);
+			Assert.AreEqual('6', game.grid[0, 2]);
+			Assert.AreEqual('7', game.grid[1, 2]);
+			Assert.AreEqual('8', game.grid[2, 2]);
+		}
+
+		[TestMethod]
+		public void CoordToCell()
+		{
+			Coord middleLeft;
+			middleLeft.x = 0;
+			middleLeft.y = 1;
+
+			Coord first;
+			first.x = 0;
+			first.y = 0;
+
+			Coord last;
+			last.x = 2;
+			last.y = 2;
+
+			Assert.AreEqual(3, TicTacToe.CoordToCell(middleLeft));
+			Assert.AreEqual(0, TicTacToe.CoordToCell(first));
+			Assert.AreEqual(8, TicTacToe.CoordToCell(last));
 
 		}
+
+		
+		[TestMethod]
+		public void CellToCoord()
+		{
+			Coord middleGot;
+			Coord middleExpected;
+
+			middleGot = TicTacToe.CellToCoord(4);
+			middleExpected.x = 1;
+			middleExpected.y = 1;
+
+			Coord expectedCoord;
+			Coord gotCoord;
+
+			Assert.AreEqual(middleExpected.x, middleGot.x);
+			Assert.AreEqual(middleExpected.y, middleGot.y);
+
+			// Idemnity tests
+			expectedCoord.x = 0;
+			expectedCoord.y = 1;
+			gotCoord = TicTacToe.CellToCoord(TicTacToe.CoordToCell(expectedCoord));
+			Assert.AreEqual(expectedCoord, gotCoord);
+
+			expectedCoord.x = 2;
+			expectedCoord.y = 0;
+			gotCoord = TicTacToe.CellToCoord(TicTacToe.CoordToCell(expectedCoord));
+			Assert.AreEqual(expectedCoord, gotCoord);
+
+			expectedCoord.x = 2;
+			expectedCoord.y = 1;
+			gotCoord = TicTacToe.CellToCoord(TicTacToe.CoordToCell(expectedCoord));
+			Assert.AreEqual(expectedCoord, gotCoord);
+		}
+
 
 		//Test methods to test ClearBoard()
 		[TestMethod]
@@ -80,5 +151,24 @@ namespace Windshield.Test
 			Assert.AreEqual(game.GetPlayerBySymbol('X'), game.playerOne);
 			Assert.AreEqual(game.GetPlayerBySymbol('O'), game.playerTwo);
 		}
+
+		[TestMethod]
+		public void InitializePlayers()
+		{
+
+		}
+
+		[TestMethod]
+		public void EndGame()
+		{
+
+		}
+
+		[TestMethod]
+		public void AISelectCell()
+		{
+
+		}
+
 	}
 }
