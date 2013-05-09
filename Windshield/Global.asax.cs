cd2a24546c950336f2c5ec1fff93605892dfaef6
hlynurarna@internet.is <hlynurarna@internet.is@ed2c307c-5ce9-fec3-88ac-91bfb60aa462>
2013-05-09 13:20:18 +0000

    Added support for signalR

--- Windshield/Global.asax.cs
+++ Windshield/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
 
 namespace Windshield
 {
@@ -17,6 +18,8 @@ namespace Windshield
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+			RouteTable.Routes.MapHubs(new HubConfiguration { EnableDetailedErrors = true });
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
