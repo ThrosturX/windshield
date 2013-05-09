781a573e08fcad2541339089ba09b49679fcfd14
sparog.throstur@gmail.com <sparog.throstur@gmail.com@ed2c307c-5ce9-fec3-88ac-91bfb60aa462>
2013-05-09 13:24:26 +0000

    Added hub

---
+++ trunk/Windshield/Controllers/GameHub.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+
+namespace Windshield.Controllers
+{
+//	[Authorize]
+	public class GameHub : Hub
+	{
+		public void Join(string groupId)
+		{
+			//			Context.User.Identity.Name
+			// Context.QueryString
+
+			Groups.Add(Context.ConnectionId, groupId);
+		}
+
+		public void ClickCell(string groupName, string cellId)
+		{
+			Clients.OthersInGroup(groupName).cellClicked(cellId);
+			//			Clients.Others.cellClicked(cellId);
+		}
+	}
+}
