f74163fa97a6de7bda412358ce2403d1fb22d50e
hlynurarna@internet.is <hlynurarna@internet.is@ed2c307c-5ce9-fec3-88ac-91bfb60aa462>
2013-05-10 18:08:32 +0000

    Adding boards to boardRepository works now.

--- trunk/Windshield.Test/TicTacToeTest.cs
+++ trunk/Windshield.Test/TicTacToeTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Windshield.Games.TicTacToe;
 using Windshield.Models;
 using Windshield.Models.Games;
 using Windshield.Test.MockObjects;
